131f399f2a1e78e97863428b8af6e2aa66fabb60
marnixah <marnix@hagehoeve.com>
2021-11-29 22:15:39 +0100

    feat: basic implementation of frontend

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let normalText = '';
+	let cyberifiedText = '';
+
+	const updateCyberifiedText = () =>
+    normalText ?
+		fetch(`/cyberify/${normalText}`)
+			.then((response) => response.text())
+			.then((resText) => (cyberifiedText = resText))
+      :
+      cyberifiedText = '';
+</script>
+
+<input type="text" bind:value={normalText} on:input={updateCyberifiedText} />
+<p>{cyberifiedText}</p>
