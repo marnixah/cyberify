d612ac46a840950d3deb9c6044678b7e7d353066
marnixah <marnix@hagehoeve.com>
2021-11-29 22:15:27 +0100

    fix: no nouns error

--- src/routes/cyberify/[text].ts
+++ src/routes/cyberify/[text].ts
@@ -4,8 +4,15 @@ const wordpos = new WordPOS();
 export async function get({ params }: { params: { text: string } }) {
 	let { text } = params;
 	// Prepend all proper nouns with 'cyber'
-	(await wordpos.getNouns(text)).forEach((noun) => {
-		text = text.replace(noun, `cyber${noun}`);
-	});
+	try {
+		(await wordpos.getNouns(text)).forEach((noun) => {
+			text = text.replace(noun, `cyber${noun}`);
+		});
+	} catch (e) {
+		// Catch read properties of undefined error
+		if (e.message.includes('read properties of undefined')) {
+			return { body: text };
+		}
+	}
 	return { body: text };
 }
